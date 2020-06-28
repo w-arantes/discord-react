804eb4443cae02bae5341db7bd77b99bd4b1b96f
w-arantes <w.aranteslfc@gmail.com>
2020-06-28 17:13:13 -0300

    feat: ChannelInfo Component

---
+++ src/components/ChannelInfo/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Container, HashtagIcon, Title, Separator, Description } from './styles';
+
+const ChannelInfo: React.FC = () => {
+  return (
+    <Container>
+      <HashtagIcon />
+
+      <Title>Chat</Title>
+
+      <Separator />
+
+      <Description>Channel open to chat and discussions about React</Description>
+    </Container>
+  );
+};
+
+export default ChannelInfo;
