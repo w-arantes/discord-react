afebde9b1485c144e79ffea0e2e2fe357e9ace70
w-arantes <w.aranteslfc@gmail.com>
2020-06-28 16:36:51 -0300

    feat: ServerName Component

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Title, ExpandIcon } from './styles';
+
+const ServerName: React.FC = () => {
+  return (
+    <Container>
+      <Title>React Server</Title>
+
+      <ExpandIcon />
+    </Container>
+  );
+};
+
+export default ServerName;
